d8d2c7cca4a815cdaa96545952ed6fc66b81377f
Michael <mike.sabat@gmail.com>
2013-03-18 22:03:29 -0400

    Adding Devise and Simple Forms

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module OMR
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    #required for devise on heroku
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
